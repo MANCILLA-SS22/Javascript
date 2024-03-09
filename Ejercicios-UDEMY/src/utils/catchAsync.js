ef8df577461764ae81b2977cc7dae19ab4d54dd5
MANCILLA-SS22 <xxel.tiradorxx@gmail.com>
2024-03-09 01:15:32 -0800

    first commit

--- Ejercicios-UDEMY/src/utils/catchAsync.js
+++
@@ -1,7 +0,0 @@
-function catchFunc(fn){
-    return function(req, res, next){ //This is the funcion that express we'll gonna call. It's here where req, res, next are recognized by express and not in the function bellow
-        return fn(req, res, next).catch(next);
-    }
-}
-
-export {catchFunc};
--- Forky_App/src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
-export const TIMEOUT_SEC = 1;
-export const RES_PER_PAGE = 10;
-export const KEY = "181a7c81-9eab-4098-b24b-253b597d8d2f";
-export const MODAL_CLOSE_SEC = 2.5;
